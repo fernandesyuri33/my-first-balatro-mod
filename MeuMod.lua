32b0ff9df69721b8ad0b3acf12afb0e4361f1a4e
Yuri Fernandes <yurifoliveira33@gmail.com>
2025-09-01 22:28:34 +0200

    add the x

--- MeuMod.lua
+++ MeuMod.lua
@@ -101,6 +101,18 @@ SMODS.Joker {
               end
             }))
 
+            G.E_MANAGER:add_event(Event({
+              trigger = 'after', delay = 0.1,
+              func = function()
+                -- mark as permanently spent and show the red X
+                card.ability.extra.used = true
+                card:set_debuff(true)                       -- <<< draws the X + disables calculate hooks
+                card:juice_up(0.4, 0.6)
+                card_eval_status_text(card, 'extra', nil, nil, nil, { message = 'Spent' })
+                return true
+              end
+            }))
+
             return true
           end
         }))
